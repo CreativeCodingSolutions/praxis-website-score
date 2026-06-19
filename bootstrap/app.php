33288e09d70f7a02711f2db2e403079a319751a9
CTO CreativeCoding <cto@creativecoding.cloud>
2026-06-19 08:50:33 +0000

    fix: register custom TrustProxies middleware in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,11 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(web: __DIR__.'/../routes/web.php', health: '/up')
-    ->withMiddleware(function (Middleware $middleware) {})
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->replace(
+            \Illuminate\Http\Middleware\TrustProxies::class,
+            \App\Http\Middleware\TrustProxies::class
+        );
+    })
     ->withExceptions(function (Exceptions $exceptions) {})
     ->create();
